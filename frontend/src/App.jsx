d62e7791bd398bd1d20d54570d12257438f9d538
BilalKayhan <fbilalkayhan@gmail.com>
2025-03-17 14:53:39 +0300

    Common areas were added to the layout folder.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,13 @@
 import './App.css'
+import Footer from './components/layout/footer/Footer'
+import Header from './components/layout/header/Header'
 
 function App() {
   return (
     <>
+    <Header/>
     <h1>E-Shopper</h1>
+    <Footer/>
     </>
   )
 }
