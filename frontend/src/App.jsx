c7bfcbd389196d937e46bfd0a69d9b1860a37b9d
BilalKayhan <fbilalkayhan@gmail.com>
2025-03-19 18:52:07 +0300

    Components created.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,26 @@
 import './App.css'
+import Categories from './components/categories/Categories'
+import Featureds from './components/featureds/Featureds'
 import Footer from './components/layout/footer/Footer'
 import Header from './components/layout/header/Header'
+import Navbar from './components/navbar/Navbar'
+import NewProducts from './components/newproducts/NewProducts'
+import Offers from './components/offers/Offers'
+import Products from './components/products/Products'
+import Subscribe from './components/subscribe/Subscribe'
+
 
 function App() {
   return (
     <>
     <Header/>
-    <h1>E-Shopper</h1>
+    <Navbar/>
+    <Featureds/>
+    <Categories/>
+    <Offers/>
+    <Products/>
+    <Subscribe/>
+    <NewProducts/>
     <Footer/>
     </>
   )
