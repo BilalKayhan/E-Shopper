1a25ca464cf379eb69f4573d048815100e2f37bc
BilalKayhan <fbilalkayhan@gmail.com>
2025-03-20 15:27:54 +0300

    Pages created and some bugs fixed.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,25 @@
 import './App.css'
-import Categories from './components/categories/Categories'
-import Featureds from './components/featureds/Featureds'
 import Footer from './components/layout/footer/Footer'
 import Header from './components/layout/header/Header'
-import Navbar from './components/navbar/Navbar'
-import NewProducts from './components/newproducts/NewProducts'
-import Offers from './components/offers/Offers'
-import Products from './components/products/Products'
-import Subscribe from './components/subscribe/Subscribe'
+import ShopPage from './pages/ShopPage'
+// import DetailPage from './pages/DetailPage'
+// import CheckoutPage from './pages/CheckoutPage'
+// import CartPage from './pages/CartPage'
+// import ContactPage from './pages/ContactPage'
+// import HomePage from './pages/HomePage'
+
 
 
 function App() {
   return (
     <>
     <Header/>
-    <Navbar/>
-    <Featureds/>
-    <Categories/>
-    <Offers/>
-    <Products/>
-    <Subscribe/>
-    <NewProducts/>
+    {/* <HomePage/> */}
+    {/* <ContactPage/> */}
+    {/* <CartPage/> */}
+    {/* <CheckoutPage/> */}
+    {/* <DetailPage/> */}
+    <ShopPage/>
     <Footer/>
     </>
   )
